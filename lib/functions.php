5c33ff897102cf3430a44907e9a7855bd91c8f17
Rodrigo Rutkoski Rodrigues <rutkoski@gmail.com>
2013-06-13 06:45:22 -0700

    Added "gerador de cpf e cnpj" to functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -673,3 +673,90 @@ if (!function_exists('date_parse_from_format')) {
     return $dt;
   }
 }
+
+/**
+ *
+ * Gerador de CPF e CNPJ
+ *
+ */
+
+function mod($dividendo, $divisor)
+{
+  return round($dividendo - (floor($dividendo / $divisor) * $divisor));
+}
+
+function sy_cpf($compontos = true, $seed = null)
+{
+  if (! is_null($seed)) {
+    mt_srand($seed);
+  }
+
+  $n1 = mt_rand(0, 9);
+  $n2 = mt_rand(0, 9);
+  $n3 = mt_rand(0, 9);
+  $n4 = mt_rand(0, 9);
+  $n5 = mt_rand(0, 9);
+  $n6 = mt_rand(0, 9);
+  $n7 = mt_rand(0, 9);
+  $n8 = mt_rand(0, 9);
+  $n9 = mt_rand(0, 9);
+  $d1 = $n9 * 2 + $n8 * 3 + $n7 * 4 + $n6 * 5 + $n5 * 6 + $n4 * 7 + $n3 * 8 + $n2 * 9 + $n1 * 10;
+  $d1 = 11 - (mod($d1, 11));
+  if ($d1 >= 10) {
+    $d1 = 0;
+  }
+  $d2 = $d1 * 2 + $n9 * 3 + $n8 * 4 + $n7 * 5 + $n6 * 6 + $n5 * 7 + $n4 * 8 + $n3 * 9 + $n2 * 10 + $n1 * 11;
+  $d2 = 11 - (mod($d2, 11));
+  if ($d2 >= 10) {
+    $d2 = 0;
+  }
+  $retorno = '';
+  if ($compontos) {
+    $retorno = '' . $n1 . $n2 . $n3 . "." . $n4 . $n5 . $n6 . "." . $n7 . $n8 . $n9 . "-" . $d1 . $d2;
+  }
+  else {
+    $retorno = '' . $n1 . $n2 . $n3 . $n4 . $n5 . $n6 . $n7 . $n8 . $n9 . $d1 . $d2;
+  }
+  return $retorno;
+}
+
+function sy_cnpj($compontos = true, $seed = null)
+{
+  if (! is_null($seed)) {
+    mt_srand($seed);
+  }
+
+  $n1 = mt_rand(0, 9);
+  $n2 = mt_rand(0, 9);
+  $n3 = mt_rand(0, 9);
+  $n4 = mt_rand(0, 9);
+  $n5 = mt_rand(0, 9);
+  $n6 = mt_rand(0, 9);
+  $n7 = mt_rand(0, 9);
+  $n8 = mt_rand(0, 9);
+  $n9 = 0;
+  $n10 = 0;
+  $n11 = 0;
+  $n12 = 1;
+  $d1 = $n12 * 2 + $n11 * 3 + $n10 * 4 + $n9 * 5 + $n8 * 6 + $n7 * 7 + $n6 * 8 + $n5 * 9 + $n4 * 2 + $n3 * 3 + $n2 * 4 +
+  $n1 * 5;
+  $d1 = 11 - (mod($d1, 11));
+  if ($d1 >= 10) {
+    $d1 = 0;
+  }
+  $d2 = $d1 * 2 + $n12 * 3 + $n11 * 4 + $n10 * 5 + $n9 * 6 + $n8 * 7 + $n7 * 8 + $n6 * 9 + $n5 * 2 + $n4 * 3 + $n3 * 4 +
+  $n2 * 5 + $n1 * 6;
+  $d2 = 11 - (mod($d2, 11));
+  if ($d2 >= 10) {
+    $d2 = 0;
+  }
+  $retorno = '';
+  if ($compontos) {
+    $retorno = '' . $n1 . $n2 . "." . $n3 . $n4 . $n5 . "." . $n6 . $n7 . $n8 . "/" . $n9 . $n10 . $n11 . $n12 . "-" .
+      $d1 . $d2;
+  }
+  else {
+    $retorno = '' . $n1 . $n2 . $n3 . $n4 . $n5 . $n6 . $n7 . $n8 . $n9 . $n10 . $n11 . $n12 . $d1 . $d2;
+  }
+  return $retorno;
+}
